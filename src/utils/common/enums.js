9dcb1e42908199657876a40e9a680a4740f93770
Rohit chaudhary <151979116+Rohit-Chaudhary86@users.noreply.github.com>
2025-06-09 16:56:57 +0530

    added booking model along with utils from flight service

---
+++ src/utils/common/enums.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE={
+    BUSINESS:'business',
+    ECONOMY:'economy',
+    PREMIUM_ECONOMY:'premium-economy',
+    FIRST_CLASS:'first-class'
+}
+
+const Booking_STATUS={
+  BOOKED:'booked',
+  CANCELLED:'cancelled',
+  INITIATED:'initiated',
+  PENDING:'pending'
+
+}
+
+module.exports={
+  SEAT_TYPE,Booking_STATUS
+}
---
+++ src/utils/common/index.js
@@ -0,0 +1,5 @@
+module.exports={
+    ErrorResponse:require('./error-Response'),
+    SuccessResponse:require('./success-Response'),
+    Enums:require('./enums')
+}
