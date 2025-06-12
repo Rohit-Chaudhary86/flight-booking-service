2349520e8265db6e730b1563e0ad7f53547472d8
Rohit chaudhary <151979116+Rohit-Chaudhary86@users.noreply.github.com>
2025-06-12 11:10:48 +0530

    Added feature to check seats are avilable or not

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,3 +1,25 @@
-module.exports={
+const axios=require('axios');
+const {StatusCodes}=require('http-status-codes');
+const{BookingRepository}=require('../repositories');
+const db=require('../models');
+const {ServerConfig}=require('../config');
+const appError = require('../utils/errors/app-Error');
+
+async function createBooking(data){
     
+    return new Promise((resolve,reject)=>{
+      const result=db.sequelize.transaction(async function bookingImpl(t){
+        const flight=await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`)
+        // console.log(flight.data);
+        const flightData=flight.data.data;
+        if(data.noOfSeats > flightData.totalSeats){
+            reject(new appError("Not enought seats avilable ",StatusCodes.BAD_GATEWAY));
+        }
+        resolve(true);
+      });
+   });  
+}
+
+module.exports={
+     createBooking
 }
